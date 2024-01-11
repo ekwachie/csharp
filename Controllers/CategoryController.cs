83640813792efb72e4b46cb9d8bbb5958907dfe3
ekwachie <myblogpaygh@gmail.com>
2024-01-11 17:57:12 +0000

    three methods of retrieving records from db with a condition

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,13 +15,15 @@ public class CategoryController : Controller
     {
         List<Category> objCategoryList = db.Categories.ToList();
 
-        if (objCategoryList == null )
+        if (objCategoryList == null)
         {
-           return View(); 
-        }else{
+            return View();
+        }
+        else
+        {
             return View(objCategoryList);
         }
-        
+
     }
 
     // for Get
@@ -51,16 +53,20 @@ public class CategoryController : Controller
         return View();
     }
 
-   // for Get
+    // for Get
     public IActionResult Edit(int? id)
     {
         if (id == null || id == 0)
         {
             return NotFound();
         }
+        // first style of retrieving data from db using GET - only works on the primary key
         Category? data = db.Categories.Find(id);
-        
-        if (data == null )
+        // second style of retrieving data from db using GET - will work if not a primary key
+        // Category? data1 = db.Categories.FirstOrDefault(u => u.Id == id);
+        // last style of retrieving data from db using GET - mainly used for complex queries
+        // Category? data2 = db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        if (data == null)
         {
             return NotFound();
         }
